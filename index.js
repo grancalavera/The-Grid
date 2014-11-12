268cd71adb79b01b656c95ec5444c32d0d87ee15
Leon Coto <leoncoto@gmail.com>
2014-11-12 08:36:35 +0000

    Making it easier to use and nicer to look

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path')
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.resolve('index.html'));
 });
+
+app.get('/static/*', function (req, res) {
+    res.sendFile(path.resolve(req.params[0]))
+})
 
 io.on('connection', function(socket){
     socket.on('toggle', function(message){
